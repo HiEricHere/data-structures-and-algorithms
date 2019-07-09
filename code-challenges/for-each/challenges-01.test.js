537066439aa6ab4d146d5c814bbc64babfb9c198
Eric Huang <ejhuang84@gmail.com>
2019-07-08 19:03:57 -0700

    challenge 4

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -40,6 +40,7 @@ const addNumbers = (num, arr, times, callback) => {
   for ( let i = 0; i < times; i++ ){
     callback(arr, num);
   }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -55,11 +56,16 @@ Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 const removeOne = (num, arr) => {
-  // Solution code here...
+  if ( ( num % 3 ) === 2 ) {
+    arr.pop();
+  }
 };
 
 const removeElements = (arr, callback) => {
-  // Solution code here...
+  for ( let i = 0; i < arr.length; i++ ){
+    callback(arr[i], arr);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -69,7 +75,10 @@ Write a function named removeWithForEach that produces the same output as challe
 ------------------------------------------------------------------------------------------------ */
 
 const removeWithForEach = (arr, callback) => {
-  // Solution code here...
+  arr.forEach( function(value) {
+    callback(value, arr);
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -83,7 +92,9 @@ This anonymous function should accept up to three arguments: the element, the in
 ------------------------------------------------------------------------------------------------ */
 
 const removeWithAnon = (arr) => {
-  // Solution code here...
+  arr.forEach( function(value, index, arr) ) {
+    var remove = function (value, index, arr)
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
